b60632cbe638b2085b46c4ef10c92e93386f8369
Park Sunjoo <86298664+55soup@users.noreply.github.com>
2022-10-23 23:55:15 +0900

    20220602 위험지역찾기, 등수구하기

---
+++ 4_9_danger_area_how2/4_9_danger_area_how2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+	int height[4][4] = { {3, 6, 2, 8}, {7, 3, 4, 2}, {8, 6, 7, 3}, {5, 3, 2, 9} };
+	int height_len = 4;
+	int ret = solution(height, height_len = 4);
+
+	printf("solution 함수의 반환 값은 %d 입니다.\n", ret);
+}
